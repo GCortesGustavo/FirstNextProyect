39e12513be4d30e57e251c1f9ef31027c10a7a63
GCortesGustavo <gustavocortes934@gmail.com>
2023-09-12 13:20:29 -0300

    First Commit

---
+++ app/about/page.jsx
@@ -0,0 +1,11 @@
+const About = () => {
+    return(
+        <div>
+            <h1>My Name is Gustavo,</h1> 
+            <p>I'm full stack developer.</p>
+            <p>I'm from Mendoza, Argentina</p>
+        </div>
+    )
+}
+
+export default About
